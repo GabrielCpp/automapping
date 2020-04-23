e6d4bc54cd1da858319003d58c9e2a264e2bde53
gabriel <gabcpp@outlook.fr>
2020-04-22 23:29:47 -0400

    Set download url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/GabrielCpp/automapping",
-    download_url="",
+    download_url="https://github.com/GabrielCpp/automapping/archive/v0.0.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
