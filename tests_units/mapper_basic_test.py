a3621c770e3b128bf9f51c070dd9483e13e8bfc6
gabriel <gabcpp@outlook.fr>
2020-05-18 13:00:31 -0400

    Test updater to vlaidate it can convert to dict directly

--- tests_units/mapper_basic_test.py
+++ tests_units/mapper_basic_test.py
@@ -1,4 +1,5 @@
-from automapping import Mapper, PartialMap, Rename, BiMorph, NaturalCopy, AutoMap
+import pytest
+from automapping import Mapper, PartialMap, Rename, BiMorph, NaturalCopy, AutoMap, ObjectDictDictUpdater, DictDictTypeUpdater
 from dataclasses import dataclass
 
 
@@ -26,6 +27,17 @@ def b_to_a(value):
     return value - 4
 
 
+@pytest.fixture
+def mapper_automap_a_b():
+    mapper = Mapper()
+    mapper.add_mapper(AutoMap(A, B, [
+        Rename('a_name', 'b_name'),
+        BiMorph('number_a', 'number_b', a_to_b, b_to_a),
+    ]))
+
+    return mapper
+
+
 def test_given_reverse_mapped_object_should_get_be_able_to_map_both_side_correctly():
     expected_mapped_object = B(b_name='Gabriel', number_b=30, a='a', b='b')
     expected_reverse_mapped_object = A(
@@ -46,20 +58,29 @@ def test_given_reverse_mapped_object_should_get_be_able_to_map_both_side_correct
     assert actual_reverse_object == expected_reverse_mapped_object
 
 
-def test_given_basic_flat_mapping_automapper_should_discover_dataclass_mapping():
+def test_given_basic_flat_mapping_automapper_should_discover_dataclass_mapping(mapper_automap_a_b):
     expected_mapped_object = B(b_name='Gabriel', number_b=30, a='a', b='b')
     expected_reverse_mapped_object = A(
         a_name='Gabriel', number_a=26, a='a', b='b')
 
-    mapper = Mapper()
-    mapper.add_mapper(AutoMap(A, B, [
-        Rename('a_name', 'b_name'),
-        BiMorph('number_a', 'number_b', a_to_b, b_to_a),
-    ]))
-
     a = A(a_name='Gabriel', number_a=26, a='a', b='b')
-    actual_object = mapper.map(a, B)
-    actual_reverse_object = mapper.map(actual_object, A)
+    actual_object = mapper_automap_a_b.map(a, B)
+    actual_reverse_object = mapper_automap_a_b.map(actual_object, A)
 
     assert actual_object == expected_mapped_object
     assert actual_reverse_object == expected_reverse_mapped_object
+
+
+def test_given_basic_object_should_it_beconvertible_to_dict(mapper_automap_a_b):
+    expected_mapped_object = {'b_name': 'Gabriel',
+                              'number_b': 30, 'a': 'a', 'b': 'b'}
+
+    expected_reverse_mapped_object = {
+        'a_name': 'Gabriel', 'number_a': 26, 'a': 'a', 'b': 'b'}
+
+    a = A(a_name='Gabriel', number_a=26, a='a', b='b')
+    actual_object = mapper_automap_a_b.map(a, B, ObjectDictDictUpdater)
+    actual_reverse_object = mapper_automap_a_b.map(
+        B(**actual_object), A, ObjectDictDictUpdater)
+
+    assert actual_object == expected_mapped_object
